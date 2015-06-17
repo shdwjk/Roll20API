e2f28fca0b3fc2da61991d491ba7bd2e989836b5
Aaron C. Meadows <shdwjk@gmail.com>
2015-06-16 22:31:49 -0500

    Updating: WeightedDice v0.3.0

--- WeightedDice/WeightedDice.js
+++ WeightedDice/WeightedDice.js
@@ -2,55 +2,55 @@
 // By:       The Aaron, Arcane Scriptomancer
 // Contact:  https://app.roll20.net/users/104025/the-aaron
 
-var WeightedDice = WeightedDice || (function(){
-    "use strict";
-    
-    var version = '0.3.0',
-        lastUpdate = 1434511712,
-        schemaVersion = 0.1,
+var WeightedDice = WeightedDice || {
+    version: 0.3,
+    schemaVersion: 0.1,
 
-    checkInstall = function() {
+    CheckInstall: function() {
         log('-=> WeightedDice v'+version+' <=-  ['+(new Date(lastUpdate*1000))+']');
 
-        if( ! _.has(state,'WeightedDice') || state.WeightedDice.version !== schemaVersion) {
+		if( ! _.has(state,'WeightedDice') || state.WeightedDice.schemaVersion != WeightedDice.schemaVersion)
             log('  > Updating Schema to v'+schemaVersion+' <');
+		{
+			/* Default Settings stored in the state. */
 			state.WeightedDice = {
 				version: WeightedDice.schemaVersion
-			};
+			}
 		}
 	},
     
-    handleInput = function(tokens) {
-        var sides = parseInt(tokens[0],10),
-            minroll = parseInt(tokens[1],10),
-            tableName,
-            tables,
-            newTable,
-            weight;
+    HandleInput: function(tokens,msg) {
+        
+        var sides = parseInt(tokens[0]);
+        var minroll = parseInt(tokens[1]);
 
         if(
             tokens.length <2 
-            || 2 !== tokens.length 
+            || 2 != tokens.length 
             || _.isNull(sides) 
             || !_.isNumber(sides) 
             || _.isNull(minroll) 
             || !_.isNumber(minroll) 
             || sides < minroll
-        ) {
+        )
+        {
             sendChat('','/w gm Usage: !weighted-die [number of sides] [minimum roll number]');
             return;
         }
         
-        tableName='d'+sides+'min'+minroll;
+        var tableName='d'+sides+'min'+minroll;
         // see if it's already defined
-        tables=findObjs({type: 'rollabletable', name: tableName});
-        if(tables.length) {
+        var tables=findObjs({type: 'rollabletable', name: tableName});
+        if(tables.length)
+        {
             sendChat('','/w gm Table '+tableName+' already exists.');
-        } else {
-            newTable=createObj('rollabletable',{name: tableName});
+        }
+        else
+        {
+            var newTable=createObj('rollabletable',{name: tableName});
             _.each(_.range(minroll,(sides+1)), function(r){
-                weight = ( (r === minroll) ? minroll : 1);
-                createObj('tableitem',{
+                var weight = ( (r == minroll) ? minroll : 1);
+                var newTableItem=createObj('tableitem',{
                     _rollabletableid: newTable.id,
                     name: r,
                     weight: weight
@@ -60,37 +60,29 @@ var WeightedDice = WeightedDice || (function(){
         }
     },
     
-    registerEventHandlers = function() {        
+    RegisterEventHandlers: function(){        
 		on("chat:message", function (msg) {
-            var tokenized, command;
 			/* Exit if not an api command */
-			if (msg.type !== "api") {
-                return;
-            }
+			if (msg.type != "api") return;
 
 
-			tokenized = msg.content.split(" ");
-			command = tokenized[0];
+			var tokenized = msg.content.split(" ");
+			var command = tokenized[0];
 
-			switch(command) {
+			switch(command)
+			{
 				case "!weighted-die":
-					if(playerIsGM(msg.playerid)) {
-						handleInput(_.rest(tokenized),msg);
+					if(playerIsGM(msg.playerid))
+					{
+						WeightedDice.HandleInput(_.rest(tokenized),msg);
 					}
 					break;
 			}
 		});
-	};
-
-    return {
-        RegisterEventHandlers: registerEventHandlers,
-        CheckInstall: checkInstall
-    };
-}());
+	}
+};
 
 on("ready",function(){
-    "use strict";
-
-    WeightedDice.CheckInstall(); 
-    WeightedDice.RegisterEventHandlers();
+	WeightedDice.CheckInstall(); 
+	WeightedDice.RegisterEventHandlers();
 });
