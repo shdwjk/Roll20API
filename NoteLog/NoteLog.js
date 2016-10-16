25976846e03e025de821ae21dbeaebced0b61065
Aaron C. Meadows <shdwjk@gmail.com>
2016-10-16 14:27:27 -0400

    Updated NoteLog to v0.1.2

--- NoteLog/NoteLog.js
+++ NoteLog/NoteLog.js
@@ -5,8 +5,8 @@
 var NoteLog = NoteLog || (function() {
     'use strict';
 
-    var version = '0.1.1',
-        lastUpdate = 1434428711,
+    var version = '0.1.2',
+        lastUpdate = 1476642083,
         schemaVersion = 0.1,
         noteLogName = 'Log',
         bulletChar = '&bullet;',
@@ -41,23 +41,66 @@ var NoteLog = NoteLog || (function() {
         }
     },
 
-    handleInput = function(msg) {
-        var args, nl;
+    handleInput = function(msg_orig) {
+        var args,
+            nl,
+            longtext,
+            msg = _.clone(msg_orig);
 
         if (msg.type !== "api" && !playerIsGM(msg.playerid)) {
             return;
         }
 
+		if(_.has(msg,'inlinerolls')){
+			msg.content = _.chain(msg.inlinerolls)
+				.reduce(function(m,v,k){
+                    var ti=_.reduce(v.results.rolls,function(m2,v2){
+                        if(_.has(v2,'table')){
+                            m2.push(_.reduce(v2.results,function(m3,v3){
+                                m3.push(v3.tableItem.name);
+                                return m3;
+                            },[]).join(', '));
+                        }
+                        return m2;
+                    },[]).join(', ');
+					m['$[['+k+']]']= (ti.length && ti) || v.results.total || 0;
+					return m;
+				},{})
+				.reduce(function(m,v,k){
+					return m.replace(k,v);
+				},msg.content)
+				.value();
+		}
+
         args = msg.content.split(/\s/);
+
         switch(args.shift()) {
+            case '!note-text': 
+                longtext=msg.content
+                    .replace(/^!note-text\b\s*/,'')
+                    .replace(/(\{\{([\s\S]*?)\}\})/g," $2 ")
+                    .trim();
+
+                nl = getNoteLog();
+                nl.get('notes', function(n){
+                    if(!_.isNull(n)){
+                        setTimeout(function(){
+                            let text=n+'<br>'+longtext;
+                            nl.set('notes',text);                        
+                        },0);
+                    }
+                });
+                break;
+
             case '!note-log':
                 nl = getNoteLog();
                 nl.get('notes', function(n){
                     if(!_.isNull(n)){
-                    setTimeout(function(){
-                        var text=n+'<br>'+bulletChar+' '+args.join(' ');
-                        nl.set('notes',text);                        
-                    },0);
+                        setTimeout(function(){
+                            let text=n+'<br>'+bulletChar+' '+args.join(' ');
+                            nl.set('notes',text);                        
+                        },0);
+                    }
                 });
                 break;
         }
