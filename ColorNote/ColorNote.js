02ac7ecbc2fa2458271aaf716ca5a46002ad6bb1
Aaron C. Meadows <shdwjk@gmail.com>
2021-05-19 11:22:57 -0400

    Updated ColorNote

--- ColorNote/ColorNote.js
+++ ColorNote/ColorNote.js
@@ -1,40 +1,61 @@
 // Github:   https://github.com/shdwjk/Roll20API/blob/master/ColorNote/ColorNote.js
 // By:       The Aaron, Arcane Scriptomancer
 // Contact:  https://app.roll20.net/users/104025/the-aaron
+var API_Meta = API_Meta||{}; //eslint-disable-line no-var
+API_Meta.ColorNote={offset:Number.MAX_SAFE_INTEGER,lineCount:-1};
+{try{throw new Error('');}catch(e){API_Meta.ColorNote.offset=(parseInt(e.stack.split(/\n/)[1].replace(/^.*:(\d+):.*$/,'$1'),10)-6);}}
 
-var ColorNote = ColorNote || (function() {
-    'use strict';
- 
-    var version = '0.2.3',
-        lastUpdate = 1606428285,
- 
-    ch = function (c) {
-        var entities = {
-			'<' : 'lt',
-			'>' : 'gt',
-			"'" : '#39',
-			'@' : '#64',
-			'{' : '#123',
-			'|' : '#124',
-			'}' : '#125',
-			'[' : '#91',
-			']' : '#93',
-			'"' : 'quot',
-			'-' : 'mdash',
-			' ' : 'nbsp'
-		};
- 
-		if(_.has(entities,c) ){
-			return ('&'+entities[c]+';');
-		}
-		return '';
-	},
+const ColorNote = (() => {  // eslint-disable-line no-unused-vars
+    const version = '0.2.4';
+    API_Meta.ColorNote.version = version;
+    const lastUpdate = 1621437645;
 
-	checkInstall = function(){
+    const processInlinerolls = (msg) => {
+        if(msg.hasOwnProperty('inlinerolls')){
+            return msg.inlinerolls
+            .reduce((m,v,k) => {
+                let ti=v.results.rolls.reduce((m2,v2) => {
+                    if(v2.hasOwnProperty('table')){
+                        m2.push(v2.results.reduce((m3,v3) => [...m3,v3.tableItem.name],[]).join(", "));
+                    }
+                    return m2;
+                },[]).join(', ');
+                return [...m,{k:`$[[${k}]]`, v:(ti.length && ti) || v.results.total || 0}];
+            },[])
+            .reduce((m,o) => m.replace(o.k,o.v), msg.content);
+        } else {
+            return msg.content;
+        }
+    };
+
+    const ch = (c) => {
+        const entities = {
+            '<' : 'lt',
+            '>' : 'gt',
+            "'" : '#39',
+            '@' : '#64',
+            '{' : '#123',
+            '|' : '#124',
+            '}' : '#125',
+            '[' : '#91',
+            ']' : '#93',
+            '"' : 'quot',
+            '*' : 'ast',
+            '/' : 'sol',
+            ' ' : 'nbsp'
+        };
+
+        if( entities.hasOwnProperty(c) ){
+            return `&${entities[c]};`;
+        }
+        return '';
+    };
+ 
+	const checkInstall = function(){
         log('-=> ColorNote v'+version+' <=-  ['+(new Date(lastUpdate*1000))+']');
-	},
+	};
  
-	showHelp = function(who) {
+	const showHelp = function(who) {
         sendChat('',
             '/w "'+who+'" '
 +'<div style="border: 1px solid black; background-color: white; padding: 3px 3px;">'
@@ -58,18 +79,19 @@ var ColorNote = ColorNote || (function() {
 	+'</div>'
 +'</div>'
             );
-    },
-	getBrightness = function (hex) {
-		var r,g,b;
+    };
+
+	const getBrightness = function (hex) {
+		let r,g,b;
 		hex = hex.replace('#', '');
 		r = parseInt(hex.substr(0, 2),16);
 		g = parseInt(hex.substr(2, 2),16);
 		b = parseInt(hex.substr(4, 2),16);
 		return ((r * 299) + (g * 587) + (b * 114)) / 1000;
-	},
+	};
 	
-	handleInput = function(msg) {
-		var args, who, color, txcolor;
+	const handleInput = function(msg) {
+		let args, who, color, txcolor;
 
 		if (msg.type !== "api") {
 			return;
@@ -77,7 +99,7 @@ var ColorNote = ColorNote || (function() {
 
 		who=(getObj('player',msg.playerid)||{get:()=>'API'}).get('_displayname');
 
-		args = msg.content.split(/\s+/);
+		args = processInlinerolls(msg).split(/\s+/);
 		switch(args[0]) {
 			case '!note':
 				if(1 === args.length) {
@@ -113,21 +135,19 @@ var ColorNote = ColorNote || (function() {
 
 				break;
 		}
-	},
+	};
  
-	registerEventHandlers = function() {
+	const registerEventHandlers = function() {
 		on('chat:message', handleInput);
 	};
+
+    on("ready",() => {
+        checkInstall();
+        registerEventHandlers();
+    });
  
 	return {
-		CheckInstall: checkInstall,
-		RegisterEventHandlers: registerEventHandlers
 	};
-}());
+})();
  
-on("ready",function(){
-	'use strict';
- 
-	ColorNote.CheckInstall();
-	ColorNote.RegisterEventHandlers();
-});
+{try{throw new Error('');}catch(e){API_Meta.ColorNote.lineCount=(parseInt(e.stack.split(/\n/)[1].replace(/^.*:(\d+):.*$/,'$1'),10)-API_Meta.ColorNote.offset);}}
